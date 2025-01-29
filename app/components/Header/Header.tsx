44ab0c11a4d7278f0e94cc9e84ccb796f98074d0
afonso-lopes-a22204080 <100075108+afonsolopes22@users.noreply.github.com>
2025-01-29 09:50:20 +0000

    pff corre

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Header() {
     return (
       <header className={styles.header}>
-        <h1>Minha Loja</h1>
+        <h1>A Minha Loja</h1>
         <nav className={styles.nav}>
           <ul>
             <li>
